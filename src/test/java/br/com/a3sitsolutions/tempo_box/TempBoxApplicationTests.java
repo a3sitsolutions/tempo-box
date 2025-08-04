8fc628342f635242e884d5e1f9736e91fd260305
Apolonio Serafim <apolonio.serafim@hotmail.com>
2025-08-04 11:40:02 -0300

    Configura perfil de testes com H2 e propriedades específicas, ajusta uso de `@ActiveProfiles` para `test` em testes e adiciona dependência H2 no `build.gradle`.

--- src/test/java/br/com/a3sitsolutions/tempo_box/TempBoxApplicationTests.java
+++ src/test/java/br/com/a3sitsolutions/tempo_box/TempBoxApplicationTests.java
@@ -2,12 +2,15 @@ package br.com.a3sitsolutions.tempo_box;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TempBoxApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Test that the application context loads successfully
 	}
 
 }
